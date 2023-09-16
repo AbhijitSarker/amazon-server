b9aff1f09bcc3c6b73e8ec07995c7d3d41b9bba9
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-17 00:35:42 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('amazon server running');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
